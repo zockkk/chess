38a9d72c9a78ca3a796fa18f792a62253bbb38fc
zockkk <73612797+zockkk@users.noreply.github.com>
2020-11-05 15:57:18 +0300

    +

--- cikls.py
+++ cikls.py
@@ -17,7 +17,7 @@ for i in range(16):
 
 for i in range(48, 64):
     k += 2 ** i
-
+#фигуры с препиской _bit - массивы битбордов для фигур соотвутственно циклы под ними - генераторы этих массивов
 rook_bit = []
 z = 0
 for k in range(64):
@@ -127,13 +127,38 @@ for matrix in koroli:
                 z += 2 ** (8 * i + j)
     king_bit.append(z)
     z = 0
-
-def legal_silence(bitboard,bitpiece,bitmove,place):
+'''
+проверка возможности тихого хода
+bitboard- битборд доски
+bitpiece- битборд ходов проверяемой фигуры
+bitmove- битборд хода, в котором 1 отмецены начальная и конечная позиция, а также все пустые клетки между ними исходя из битборда фигуры
+place- место самой фигуры, взятое из массива коордитак фигур
+'''
+'''def legal_silence(bitboard,bitpiece,bitmove,place):
     bitplace=(place[0]-1)*8+place[1]-1
     if bitmove&bitpiece!=bitmove or bitmove&bitboard!=bitplace:
         return False
     return True
 
-def legal_eat(bitboard,bitpiece,eat,place1,place2):
+проверка возможности взятия
+eat - аналог bitmove 
+place1, place2 - координаты начальной и конечной позиции("сьедаемой" фигуры) фигуры соответственно
+
+def legal_eat(bitboard,bitpiece,biteat,place1,place2):
     place1_bit=(place1[0]-1)*8+place1[1]-1
     place2_bit = (place2[0] - 1) * 8 + place2[1] - 1
+    if bitboard&bitpiece&biteat!=place1_bit|place2_bit:
+        return False
+    return True
+
+def danger(bitboard,kingplace,)'''
+
+def legal_steps(place, piece_bit, bitboard):
+    bitpiece=piece_bit[(place[0]-1)*8+place[1]-1]
+    place_bit=2**((place[0]-1)*8+place[1]-1)
+    return bitpiece^bitboard|place_bit
+
+def legal_eat(place, piece_bit, bitboard):
+    bitpiece = piece_bit[(place[0] - 1) * 8 + place[1] - 1]
+    place_bit = 2 ** ((place[0] - 1) * 8 + place[1] - 1)
+    step=
